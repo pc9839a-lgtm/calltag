d52e26028b45644b7363ec0a5b70984d062f009a
pc9839a-lgtm <pc9839a@naver.com>
2026-07-30 14:43:21 +0900

    crm: add customizable stage model

---
+++ app/src/main/java/kr/pagero/calltag/StageOption.java
@@ -0,0 +1,13 @@
+package kr.pagero.calltag;
+
+public final class StageOption {
+    public final long id;
+    public final String name;
+    public final int position;
+
+    public StageOption(long id, String name, int position) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+    }
+}
