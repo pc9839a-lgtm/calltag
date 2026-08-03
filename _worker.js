80d00b0710524cc2b0de0d2928a46e47e0a6d1f0
pc9839a-lgtm <pc9839a@naver.com>
2026-08-03 15:40:35 +0900

    Bump deployed horizontal impact animation version

--- _worker.js
+++ _worker.js
@@ -43,7 +43,7 @@ export default {
         'calltag-stability-fix.js?v=20260802-stability1',
         'calltag-horizontal-clean.js?v=20260803-compact1',
         'calltag-horizontal-guard.js?v=20260803-pin3',
-        'calltag-horizontal-impact.js?v=20260803-impact1',
+        'calltag-horizontal-impact.js?v=20260803-impact2',
         'calltag-section-motion.js?v=20260803-motion1'
       ];
       body = body.replace('</body>', scripts.map(src => `<script src="/assets/${src}"></script>`).join('') + '</body>');
@@ -53,7 +53,7 @@ export default {
     ['content-encoding','content-length','etag','last-modified','content-md5','digest'].forEach(name => headers.delete(name));
     headers.set('content-type', 'text/html; charset=UTF-8');
     headers.set('cache-control', 'no-cache, no-store, must-revalidate');
-    headers.set('x-calltag-worker', 'v48-horizontal-impact');
+    headers.set('x-calltag-worker', 'v49-horizontal-impact2');
 
     return new Response(body, {
       status: response.status,
