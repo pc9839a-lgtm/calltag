91e24a820878e5852c936c4588407c904c144128
pc9839a-lgtm <pc9839a@naver.com>
2026-08-03 16:36:34 +0900

    Apply requested Pagero copy after intro loads

--- web/assets/calltag-pagero-intro-loader.js
+++ web/assets/calltag-pagero-intro-loader.js
@@ -1,1 +1,18 @@
-(()=>{if(document.documentElement.dataset.ctPageroIntroLoader)return;document.documentElement.dataset.ctPageroIntroLoader='1';const s=document.createElement('script');s.src='https://calltag.pagero.kr/assets/calltag-pagero-intro.js?v=20260802-8';s.defer=true;document.head.append(s);})();
+(()=>{
+  if(document.documentElement.dataset.ctPageroIntroLoaderV9)return;
+  document.documentElement.dataset.ctPageroIntroLoaderV9='1';
+
+  const applyCopy=()=>{
+    const copy=document.querySelector('#ct-pagero-intro .ct-v8-head>strong');
+    if(copy)copy.innerHTML='페이지로에서 문의를 받으면<br>관리는 콜태그 앱에서!';
+  };
+
+  const script=document.createElement('script');
+  script.src='https://calltag.pagero.kr/assets/calltag-pagero-intro.js?v=20260803-9';
+  script.defer=true;
+  script.addEventListener('load',()=>{
+    requestAnimationFrame(applyCopy);
+    setTimeout(applyCopy,250);
+  });
+  document.head.append(script);
+})();
