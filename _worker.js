cc807f8485897526867aee6de2752dd8316d7e7c
pc9839a-lgtm <pc9839a@naver.com>
2026-08-03 20:34:42 +0900

    Load visible Pagero heading fix from root worker

--- _worker.js
+++ _worker.js
@@ -33,6 +33,7 @@ export default {
         'calltag-pricing-redesign.js?v=20260801-37',
         'calltag-pagero-intro.js?v=20260803-intro11',
         'calltag-pagero-reveal-fix.js?v=20260802-reveal3',
+        'calltag-pagero-heading-fix.js?v=20260803-heading1',
         'calltag-pagero-connect-visual.js?v=20260802-connect1',
         'calltag-pagero-industries.js?v=20260802-industries2',
         'calltag-pagero-industries-two-column.js?v=20260802-three-card1',
@@ -59,7 +60,7 @@ export default {
     ['content-encoding','content-length','etag','last-modified','content-md5','digest'].forEach(name=>headers.delete(name));
     headers.set('content-type','text/html; charset=UTF-8');
     headers.set('cache-control','no-cache, no-store, must-revalidate');
-    headers.set('x-calltag-worker','v57-intro-wrapper');
+    headers.set('x-calltag-worker','v58-visible-heading');
 
     return new Response(body, {
       status: response.status,
