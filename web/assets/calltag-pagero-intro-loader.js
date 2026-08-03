c3d41408f01855adf86a48da20117b131df4926d
pc9839a-lgtm <pc9839a@naver.com>
2026-08-03 19:22:49 +0900

    Point web loader to new Pagero intro wrapper

--- web/assets/calltag-pagero-intro-loader.js
+++ web/assets/calltag-pagero-intro-loader.js
@@ -1,18 +1,8 @@
 (()=>{
-  if(document.documentElement.dataset.ctPageroIntroLoaderV9)return;
-  document.documentElement.dataset.ctPageroIntroLoaderV9='1';
-
-  const applyCopy=()=>{
-    const copy=document.querySelector('#ct-pagero-intro .ct-v8-head>strong');
-    if(copy)copy.innerHTML='페이지로에서 문의를 받으면<br>관리는 콜태그 앱에서!';
-  };
-
+  if(document.documentElement.dataset.ctPageroIntroLoaderV11)return;
+  document.documentElement.dataset.ctPageroIntroLoaderV11='1';
   const script=document.createElement('script');
-  script.src='https://calltag.pagero.kr/assets/calltag-pagero-intro.js?v=20260803-9';
+  script.src='/assets/calltag-pagero-intro.js?v=20260803-intro11';
   script.defer=true;
-  script.addEventListener('load',()=>{
-    requestAnimationFrame(applyCopy);
-    setTimeout(applyCopy,250);
-  });
   document.head.append(script);
 })();
