65bdd5decff38106f75f5e6dadb907ba4f837ee2
pc9839a-lgtm <pc9839a@naver.com>
2026-08-05 00:23:22 +0900

    Revert web PageRo section heading changes

--- web/assets/calltag-pagero-heading-fix.js
+++ web/assets/calltag-pagero-heading-fix.js
@@ -2,7 +2,7 @@
   if(document.documentElement.dataset.ctPageroHeadingFix)return;
   document.documentElement.dataset.ctPageroHeadingFix='1';
 
-  const desired='랜딩페이지에서 받은 문의,<br><span>콜태그에서 바로 관리하세요.</span>';
+  const desired='페이지로에서 문의를 받으면<br><span>관리는 콜태그 앱에서!</span>';
 
   const apply=()=>{
     const heading=document.querySelector('#ct-pagero-intro .ct-pagero-connect h2');
