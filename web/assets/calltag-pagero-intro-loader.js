f40f357d162eb8662e95407533ae0c044a73729b
pc9839a-lgtm <pc9839a@naver.com>
2026-08-07 23:37:01 +0900

    Remove obsolete PageRo intro loader hop

--- web/assets/calltag-pagero-intro-loader.js
+++
@@ -1,8 +0,0 @@
-(()=>{
-  if(document.documentElement.dataset.ctPageroIntroLoaderV11)return;
-  document.documentElement.dataset.ctPageroIntroLoaderV11='1';
-  const script=document.createElement('script');
-  script.src='/assets/calltag-pagero-intro.js?v=20260803-intro11';
-  script.defer=true;
-  document.head.append(script);
-})();
